587eebe1d52bf6804ce813c09cff7ed4943d24eb
Jordan Bohmbach <jordan.bohmbach@gmail.com>
2021-07-27 10:55:51 -0500

    set up all the route handlers but didn't implement any. made the app use them and made the blank pug file

---
+++ routes/questions.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const router = express.Router();
+const { Question, Answer, QuestionComment, AnswerComment } = require('../db/models')
+const { asyncHandler, csrfProtection, userValidators, loginValidators, handleValidationErrors } = require('./utils');
+
+/* GET the questions page to view the top questions */
+router.get('/', asyncHandler(async (req, res, next) => {
+    res.send('here in /questions GET route handler to see the home page of recent questions');
+}));
+
+/* GET the questions page to view the top questions */
+router.get('/:id(\\d+)', asyncHandler(async (req, res, next) => {
+    res.send(`Here in the /questions/${req.params.id} GET route handler to see a specific question`);
+}));
+
+/* GET the questions page to view the top questions */
+router.get('/new', asyncHandler(async (req, res, next) => {
+    res.send('here in /questions/new GET route handler to make a new question');
+}));
+
+/* Submit an answer to a specific question which will be dynamically added to the answers list */
+router.post('/:id(\\d+)/answers', asyncHandler(async (req, res, next) => {
+    res.send(`Here in the /questions/${req.params.id}/answers POST route handler to answer a specific question`);
+}));
+
+/* Submit an commment to a specific question which will be dynamically added to the answers list */
+router.post('/:id(\\d+)/comments', asyncHandler(async (req, res, next) => {
+    res.send(`Here in the /questions/${req.params.id}/comments POST route handler to comment on a specific question`);
+}));
+
+/* Update a specific question by id */
+router.put('/:id(\\d+)', asyncHandler(async (req, res, next) => {
+    res.send(`Here in the /questions/${req.params.id} PUT route handler to update a specific question`);
+}));
+
+/* Delete a specific question by id */
+router.delete('/:id(\\d+)', asyncHandler(async (req, res, next) => {
+    res.send(`Here in the /questions/${req.params.id} DELETE route handler to delete a specific question`);
+}));
+
+module.exports = router;
