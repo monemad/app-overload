ec09acac4874ef6cb77507f2344b0d7165f5b5de
Alex Monson <monson.alex@rocketmail.com>
2021-07-28 08:09:16 -0700

    working on the comments routes

--- routes/comments.js
+++ routes/comments.js
@@ -1,23 +1,18 @@
 const express = require('express');
 const bcrypt = require('bcryptjs')
 const router = express.Router();
-const { AnswerComment, QuestionComment } = require('../db/models')
+const { AnswerComment, QuestionComment , Question , Answer } = require('../db/models')
 const { asyncHandler, csrfProtection, userValidators, loginValidators, handleValidationErrors } = require('./utils');
 const { validationResult } = require('express-validator');
 
 
 
-router.get('/', csrfProtection, asyncHandler(async (req, res) => {
-    // const validationErrors = validationResult(req)
-    const { comment } = req.body;
-    const answerComment = AnswerComment.build({ comment })
-  }));
-
-
 router.get('/:id(\\d+)/edit', csrfProtection, asyncHandler(async (req, res) => {
-    const Id = parseInt(req.params.id, 10);
-    const answerComment = await AnswerComment.findByPk();
-    res.render('comment-edit', { title: 'Edit Comment', answerCommentId, csrfToken: req.csrfToken(),});
+    const commentId = parseInt(req.params.id, 10);
+    const answerComment = await AnswerComment.findByPk(commentId);
+
+    //show same text area box
+    res.render('comment-edit', { title: 'Edit Comment', answerCommentId, csrfToken: req.csrfToken()});
 
   }));
 
@@ -37,5 +32,33 @@ router.post('/:id(\\d+)/delete', csrfProtection, asyncHandler(async (req, res) =
     res.redirect('/');
 
   }));
+
+
+router.get('/:id(\\d+)/edit', csrfProtection, asyncHandler(async (req, res) => {
+    const Id = parseInt(req.params.id, 10);
+    const questionComment = await QuestionComment.findByPk();
+
+    //show same text area box
+    res.render('comment-edit', { title: 'Edit Comment', questionCommentId, csrfToken: req.csrfToken()});
+
+  }));
+
+router.post('/:id(\\d+)', csrfProtection, asyncHandler(async (req, res) => {
+    const newComment = req.body.answer
+    const commentId = parseInt(req.params.id, 10);
+    const comment = await QuestionComment.findByPk(commentId);
+    comment.update({comment: newComment})
+
+  }));
+
+
+router.post('/:id(\\d+)/delete', csrfProtection, asyncHandler(async (req, res) => {
+    const commentId = parseInt(req.params.id, 10);
+    const questionComment = await QuestionComment.findByPk(commentId);
+    await questionComment.destroy();
+    res.redirect('/');
+
+  }));
+
 
   module.exports = router;
